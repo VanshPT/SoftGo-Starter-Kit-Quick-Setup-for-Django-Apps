363228f36efcab0c4891f67319ceaa930963a02a
Vansh Thakkar <vanshpthakkar@gmail.com>
2024-09-27 21:09:23 +0530

    instructions to add multiple pages added

--- home/urls.py
+++ home/urls.py
@@ -4,6 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.landing, name="landing"),
-    path('dashboard/<str:username>/', views.render_dashboard, name="render_dashboard")
+    path('dashboard/<str:username>/', views.render_dashboard, name="render_dashboard"),
+    path('auto_clustering/<str:username>/', views.auto_clustering, name="auto_clustering"),
+    path('explore_topics/<str:username>/', views.explore_topics, name="explore_topics"),
+    path('search_papers/<str:username>/', views.search_papers, name="search_papers"),
     
 ]
--- home/views.py
+++ home/views.py
@@ -10,3 +10,21 @@ def render_dashboard(request,username):
     user=request.user
     context={'username':user.username, 'first_name':user.first_name, 'last_name':user.last_name, 'email': user.email}
     return render(request, 'home/dashboard_main.html',context)
+
+@login_required
+def auto_clustering(request,username):
+    user=request.user
+    context={'username':user.username, 'first_name':user.first_name, 'last_name':user.last_name, 'email': user.email}
+    return render(request, 'home/render_auto_clustering.html',context)
+
+@login_required
+def explore_topics(request,username):
+    user=request.user
+    context={'username':user.username, 'first_name':user.first_name, 'last_name':user.last_name, 'email': user.email}
+    return render(request, 'home/render_explore_topics.html',context)
+
+@login_required
+def search_papers(request,username):
+    user=request.user
+    context={'username':user.username, 'first_name':user.first_name, 'last_name':user.last_name, 'email': user.email}
+    return render(request, 'home/render_search_papers.html',context)
